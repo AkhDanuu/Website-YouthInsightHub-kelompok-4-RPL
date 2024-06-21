d3136e854a36ebf740beac7bf2c16d1343a5b4e3
meyliiin <meylinda170504@gmail.com>
2024-06-21 16:01:19 +0700

    Membuat tampilan web  buku

---
+++ Tampilan_Buku.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<img src="logo (1).png" width="190px" height="50px">
+	<title>YouthInsight</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Sign In</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section class="hero">
+		<div class="container">
+			<h1>Best Books to Read on a Rainy Day!</h1>
+			<p>Find your next great read from our curated selection.</p>
+		</div>
+	</section>
+
+	<section class="books-section">
+		<div class="container">
+			<h2>Featured Books</h2>
+			<div class="books-container">
+				<div class="book">
+					<img src="Buku 1.png" alt="Book Cover 1">
+					<h3>The Wheels of Chance</h3>
+					<p>By H.G. Weles</p>
+					<button class="button"><a href="https://zlibrary.to/filedownload/the-wheels-of-chance-1">Download</a></button>
+				</div>
+				<div class="book">
+					<img src="buku 2.jpg" alt="Book Cover 2">
+					<h3>That Liverpool Girl</h3>
+					<p>By Ruth Hamilton</p>
+					<button class="button"><a href="https://zlibrary.to/filedownload/that-liverpool-girl-0">Download</a><button>
+				</div>
+				<div class="book">
+					<img src="buku 3.jpg" alt="Book Cover 3">
+					<h3>Filosofi Teras</h3>
+					<p>By DR.A. Setyo Wibowo</p>
+					<button class="button">Download</button>
+				</div>
+				<div class="book">
+					<img src="buku 4.png" alt="Book Cover 4">
+					<h3>Berani Tidak Disukai</h3>
+					<p>By Ichiro Kishimi</p>
+					<button  class="button"><a href="https://zlibrary.to/filedownload/the-wheels-of-chance-1">Download</a></button>
+				</div>
+				<div class="book">
+					<img src="buku 5.jpeg" alt="Book Cover 5">
+					<h3>Kamu Gak Sendiri</h3>
+					<p>By Syahid Muhammad</p>
+					<button class="button">Download</button>
+				</div>
+				<div class="book">
+					<img src="Buku 6.jpg" alt="Book Cover 6">
+					<h3>Mindset</h3>
+					<p>By Carrol S.Dweck, Ph.D.</p>
+					<button class="button"><a href="https://zlibrary.to/dl/mindset-4">Download</a></button>
+				</div>
+
+				<!-- Add more book items here -->
+			</div>
+		</div>
+	</section>
+
+	<footer class="footer">
+		<div class="container">
+			<p>&copy; 2024 YouthInsight</p>
+			<div class="social-icons">
+				<a href="#"><i class="fab fa-instagram"></i></a>
+				<a href="#"><i class="fab fa-facebook-f"></i></a>
+				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+		</div>
+	</footer>
+
+</body>
+</html>
